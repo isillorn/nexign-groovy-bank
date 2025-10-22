40a79194232f1e8e895583a6c16b4b22e6540987
EZ <kholodoff@gmail.com>
2025-10-22 13:55:42 +0800

    3

--- src/dev/TestClass.groovy
+++
@@ -1,7 +0,0 @@
-package dev
-
-class TestClass {
-    static void main(String[] args) {
-        print("(dev) hello!")
-    }
-}
--- src/test/GoodbyeWorld.groovy
+++
@@ -1,7 +0,0 @@
-package test
-
-class GoodbyeWorld {
-    static void main(String[] args) {
-        print("(test) goodbye!")
-    }
-}
